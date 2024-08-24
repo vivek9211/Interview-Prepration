d143c820012e747ba083dfc2c20f210f55772aa8
vivek9211 <82077595+vivek9211@users.noreply.github.com>
2024-08-24 11:30:46 +0530

    completed print

--- Recursion/print-one-to-n.cpp
+++ Recursion/print-one-to-n.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 
+void printNumber(int n) {
+    if(n==0) return;
+    printNumber(n-1);
+    cout<<n<<" ";
+}
+
 int main() {
-    cout<<"This demonstration of code.";
+    cout<<"Enter a number that you want to print: ";
+    int n;
+    cin>>n;
+    printNumber(n);
+    return 0;
 }
